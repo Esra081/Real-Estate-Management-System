0dfa932d3e5fb40bb036c170a7850c23f0eb25f8
Esra081 <esraklc081@gmail.com>
2026-09-18 15:15:14 +0300

    chore: gereksiz dosyalari kaldir (BUGUN_YAPILANLAR.md, REMS_Proje_Plani.md, backend.log, run_backend.bat)

--- run_backend.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set ASPNETCORE_URLS=https://localhost:7195;http://localhost:5107
-set ASPNETCORE_ENVIRONMENT=Development
-cd /d C:\Users\Esra\Desktop\Esra\Projects\OdakGIS_Staj\REMS\REMS.API\REMS.API
-dotnet run --launch-profile https
